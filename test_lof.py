ecee86446e62d3d5f98cfef2ae65a8b3c0af08ca
Damjan Kužnar <damjan.kuznar@ijs.si>
2016-07-21 09:36:31 +0200

    Added additional unit test.

--- test_lof.py
+++ test_lof.py
@@ -37,6 +37,26 @@ def test_reachability_distance():
 def test_outliers():
     lof.outliers(1, instances)
 
+def test_normalization_problems():
+    # see issue https://github.com/damjankuznar/pylof/issues/7
+    instances = [(1.,2.,3.),(2.,3.,4.),(1.,2.,4.),(1.,2.,1.)]
+    l = lof.outliers(1, instances)
+
+def test_duplicate_instances():
+    instances = (
+        (1, 1, 1, 1),
+        (2, 2, 2, 2),
+        (3, 3, 3, 3),
+        (2, 1, 1, 1),
+        (1, 2, 1, 1),
+        (2, 2, 2, 2),
+        (2, 2, 2, 2),
+        (1, 1, 1, 1),
+        (1, 1, 1, 1),
+        (13, 13, 13, 13)
+        )
+    lof.outliers(1, instances)
+    
 if __name__ == "__main__":
     print("Running tests, nothing more should appear if everything goes well.")
     test_LOF_normalize_instances()
@@ -44,3 +64,4 @@ if __name__ == "__main__":
     test_k_distance()
     test_reachability_distance()
     test_outliers()
+    test_normalization_problems()
